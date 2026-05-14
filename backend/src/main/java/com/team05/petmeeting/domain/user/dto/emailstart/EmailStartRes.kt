97cfc9e74821447278416ab6c4905ac00dcdf5ab
JW-1126 <jwyoon1126@naver.com>
2026-05-14 15:27:28 +0900

    refactor: EmailStartRes DTO 코틀린으로 변환

---
+++ backend/src/main/java/com/team05/petmeeting/domain/user/dto/emailstart/EmailStartRes.kt
@@ -0,0 +1,10 @@
+package com.team05.petmeeting.domain.user.dto.emailstart
+
+data class EmailStartRes(
+    val exists: Boolean,
+    val nextStep: NextStep
+) {
+    enum class NextStep {
+        SIGNUP_WITH_OTP, LOGIN_PASSWORD, SOCIAL_LOGIN_ONLY
+    }
+}
