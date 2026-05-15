d57df035cfb76f5b128387601084735ff1ee8945
alicek0 <alicekangisy@gmail.com>
2026-05-15 17:06:42 +0900

    refactor: DonationControllerTest 수정 - SpringBootTest 대신 WebMVC 사용 - csrf, 유저 인증 해서 api 보내기 테스트

--- backend/src/test/java/com/team05/petmeeting/domain/donation/controller/DonationControllerTest.java
+++ backend/src/test/java/com/team05/petmeeting/domain/donation/controller/DonationControllerTest.java
@@ -11,31 +11,41 @@ import com.team05.petmeeting.global.security.test.WithCustomUser;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@ActiveProfiles("test")
-@WithCustomUser(userId = 100L)
+@WebMvcTest(
+        controllers = DonationController.class,
+        excludeAutoConfiguration = {SecurityAutoConfiguration.class},
+        excludeFilters = @ComponentScan.Filter(
+                type = FilterType.REGEX,
+                pattern = "com\\.team05\\.petmeeting\\.global\\.security\\..*"
+        )
+)
+@WithCustomUser(userId = 100L)  // 이미 로그인 된 사용자 만들어주는 것. api 사용하는 건 인증 된 사용자여야 해서 필요함..
 public class DonationControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+    // @Autowired
+    // private ObjectMapper objectMapper;
+    // JacksonConfig.java   ← 이게 ObjectMapper 빈을 등록하고 있을 것
+    // 근데 global/security 파일들 빈으로 로딩 안 해서 수동으로 만들어줘야 함
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @MockitoBean
     private DonationService donationService;
@@ -49,6 +59,7 @@ public class DonationControllerTest {
         when(donationService.prepare(eq(100L), any(PrepareReq.class))).thenReturn(res);
 
         mvc.perform(post("/api/v1/donations/prepare")
+                        .with(csrf())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(req)))
                 .andExpect(status().isOk())
@@ -65,6 +76,7 @@ public class DonationControllerTest {
         when(donationService.donate(eq(100L), any(CompleteReq.class))).thenReturn(res);
 
         mvc.perform(post("/api/v1/donations/complete")
+                        .with(csrf())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(req)))
                 .andExpect(status().isOk())
